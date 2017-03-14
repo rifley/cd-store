2fe1747db5dbde1a67a17adcc5f2cfb5e16de732
Riley Watts <rileywatts.yes@gmail.com>
2017-03-14 15:28:20 -0700

    add CD class and initial app

---
+++ CD.java
@@ -0,0 +1,13 @@
+public class CD {
+  public String mArtist;
+  public String mAlbum;
+  public int mReleaseYear;
+  public int mPrice;
+
+  public CD(String artist, String album, int releaseYear, int price) {
+    mArtist = artist;
+    mAlbum = album;
+    mReleaseYear = releaseYear;
+    mPrice = price;
+  }
+}
